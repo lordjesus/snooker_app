56f2f487431e7cc196c7f8e0316119843c6a05e2
Daniel Jallov <danieljallov@gmail.com>
2013-10-01 21:25:21 +0200

    Finish sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+	def new 
+	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			# Sign in
+			sign_in user
+			redirect_to user
+		else 
+			# Error
+			flash.now[:error] = 'Forkert email/password kombination'
+			render 'new'
+		end		
+	end
+
+	def destroy
+		sign_out
+		redirect_to root_url
+	end
+end
