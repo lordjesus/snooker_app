ae5e0e5852fea03a867235bdc55f07fdff77196f
Daniel Jallov <danieljallov@gmail.com>
2013-10-02 23:32:18 +0200

    Update users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,13 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
+		if !user			
+			user = User.find_by(username: params[:session][:email].downcase)
+		end
 		if user && user.authenticate(params[:session][:password])
 			# Sign in
 			sign_in user
-			redirect_to user
+			redirect_back_or user
 		else 
 			# Error
 			flash.now[:error] = 'Forkert email/password kombination'
@@ -20,4 +23,14 @@ class SessionsController < ApplicationController
 		sign_out
 		redirect_to root_url
 	end
+
+	def redirect_back_or(default)
+		redirect_to(session[:return_to] || default)
+		session.delete(:return_to)
+	end
+
+	def store_location
+		session[:return_to] = request.url if request.get
+	end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
   	@user = User.find(params[:id])
   end
 
   def new
   	@user = User.new
+  end
+
+  def edit  	
+  end
+
+  def update  	
+  	if @user.update_attributes(user_params)
+  		flash[:success] = "Brugerprofil opdateret"
+  		redirect_to @user 
+  	else
+  		render 'edit'
+  	end
   end
 
   def create
@@ -24,4 +39,21 @@ class UsersController < ApplicationController
   		params.require(:user).permit(:username, :email, :password, 
   			:password_confirmation)
   	end
+
+  	# Before filters
+  	def signed_in_user
+  		unless signed_in?
+  			store_location
+  			flash[:notice] = "Log venligst ind."
+  			redirect_to signin_url
+  		end
+  	end
+
+  	def correct_user
+  		@user = User.find(params[:id])
+  		redirect_to(root_url) unless current_user?(@user)
+  	end
+  			
+  		
+
 end
