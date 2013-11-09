d7f9f2244607e5078c7da6deba6d74e36cc1ba81
Daniel Jallov <danieljallov@gmail.com>
2013-11-09 16:01:40 +0100

    Added default profile picture FIX

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,7 @@ class Profile < ActiveRecord::Base
 	belongs_to :player
 
 	has_attached_file :avatar, styles: {
-		medium: '300x300'
+		medium: '300x300',
 		thumb: '100x100>'
 	}, :default_url => "/profiles/avatars/:style/missing.png"
 end
