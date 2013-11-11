22d991a26941329d7e9c789bdd96fc759d7d5056
Daniel Jallov <danieljallov@gmail.com>
2013-11-11 23:34:57 +0100

    ActionMailer url fix

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,14 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+ActionMailer::Base.default_url_options{ :host => 'snookerdk.herokuapp.com' }
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'snookerdk.heroku.com',
+  :domain         => 'snookerdk.herokuapp.com',
   :enable_starttls_auto => true
 }
 
