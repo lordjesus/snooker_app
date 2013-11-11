44b9bf1d3c3ba9ca8736963833704ff795283eb1
Daniel Jallov <danieljallov@gmail.com>
2013-11-11 23:40:19 +0100

    Added gmail account

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-ActionMailer::Base.default_url_options{ :host => 'snookerdk.herokuapp.com' }
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'snookerdk.herokuapp.com',
-  :enable_starttls_auto => true
-}
+
 
 # Initialize the Rails application.
 SnookerApp::Application.initialize!
